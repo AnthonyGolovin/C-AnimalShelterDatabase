608456b64f31fe42dbe42d86aeac534efd0a6fa7
Anthony Golovin and Vera Weikel <anthony+vera@>
2020-01-14 16:37:34 -0800

    Updates to sql.

--- AnimalShelter/animal_shelter_db.sql
+++ AnimalShelter/animal_shelter_db.sql
@@ -1,3 +1,13 @@
+-- DROP DATABASE IF EXISTS `animal_shelter_db`;
+-- CREATE DATABASE `animal_shelter_db`;
+-- USE `animal_shelter_db`;
+
+-- DROP TABLE IF EXISTS `animals`;
+
+-- CREATE TABLE `animals` (`AnimalId` int(11) NOT NULL AUTO_INCREMENT,   `Type` varchar(255) NOT NULL ASC,   `Name` varchar(45) DEFAULT NULL,    `ADate` datetime DEFAULT NULL,    `Gender` varchar(45) DEFAULT NULL,   `Breed` varchar(45) DEFAULT NULL,   PRIMARY KEY (`AnimalId`) );
+
+-- SELECT`type` FROM `animals` ORDER BY `type` ASC;
+
 -- MySQL dump 10.13  Distrib 8.0.16, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: animal_shelter_db.sql
@@ -15,31 +25,45 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
+--USE animal_shelter_db;
 -- Table structure for table `new_table`
 --
+-- DROP DATABASE IF EXISTS `animal_shelter_db`;
+-- CREATE DATABASE `animal_shelter_db`;
+-- USE `animal_shelter_db`;
+
+-- DROP TABLE IF EXISTS `animals`;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+--  SET character_set_client = utf8mb4 ;
+-- CREATE TABLE `animals` (
+--   `AnimalId` int(11) NOT NULL AUTO_INCREMENT,
+--   `Type` varchar(255) NOT NULL ASC, /* ASC*/
+--   `Name` varchar(45) DEFAULT NULL,
+--   `Date` datetime DEFAULT NULL, /* DESC*/
+--   `Gender` varchar(45) DEFAULT NULL,
+--   `Breed` varchar(45) DEFAULT NULL,
+--   PRIMARY KEY (`AnimalId`)
+-- ) 
 DROP DATABASE IF EXISTS `animal_shelter_db`;
 CREATE DATABASE `animal_shelter_db`;
 USE `animal_shelter_db`;
-
 DROP TABLE IF EXISTS `animals`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `animals` (
-  `AnimalId` int(11) NOT NULL AUTO_INCREMENT,
-  `Type` varchar(255) DEFAULT NULL ASC, /* ASC*/
-  `Name` varchar(45) DEFAULT NULL,
-  `Date` datetime DEFAULT NULL, /* DESC*/
-  `Gender` varchar(45) DEFAULT NULL,
-  `Breed` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`AnimalId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `animals` (`AnimalId` int(11) NOT NULL AUTO_INCREMENT, 
+    `Type` varchar(255) NOT NULL ,
+    `Name` varchar(45) DEFAULT NULL,
+    `Date` datetime DEFAULT NULL,
+    `Gender` varchar(45) DEFAULT NULL,
+    `Breed` varchar(45) DEFAULT NULL, 
+    PRIMARY KEY (`AnimalId`) ); 
+    
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `new_table`
 --
-
+SELECT * FROM animals ORDER BY `type` ASC;
 LOCK TABLES `new_table` WRITE;
 /*!40000 ALTER TABLE `new_table` DISABLE KEYS */;
 /*!40000 ALTER TABLE `new_table` ENABLE KEYS */;
--- AnimalShelter/animals_shelter_db.sql
+++
@@ -1,4 +0,0 @@
-CREATE PROCEDURE `new_procedure` ()
-BEGIN
-SELECT`type`() FROM `animals` ORDER BY `type`() ASC;
-END
